4a8bb21f1d4f2801d025796bbd91e44b9dc7b6ec
Naief Chowdhury <rjsanto262@gmail.com>
2021-07-26 00:19:43 +0600

    My Work

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const shortUrl = require("./models/short.js");
+
+mongoose.connect("mongodb://localhost/family2", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({
+    extended: false
+}));
+
+
+app.get("/", async(req, res) => {
+    const shortUrls = await shortUrl.find();
+    res.render("index.ejs", {
+        shortUrls: shortUrls
+    });
+});
+
+app.post("/", async(req, res) => {
+    await shortUrl.create({
+        fullUrl: req.body.fullUrl
+    });
+    res.redirect("/");
+});
+
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Server is running at 8080");
+});
